22cecbbf49353c883263d253a951c1d9b141c4fb
Diego <fariia_di@hotmail.com>
2022-07-15 20:19:49 -0300

    autenticacao de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 import { Credenciais } from '../models/credenciais';
 
 @Component({
@@ -18,22 +20,26 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   logar(){
-    this.toast.error('Usuario e/ou senha inválidos', 'Login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate[''];
+    }, () => {
+      this.toast.error('Usuário e/ou Senha Inválidos');
+    })
   }
 
   validaCampos(): boolean{
-    if(this.email.valid && this.senha.valid){
-      return true;
-    } else {
-      return false;
-    }
+      return this.email.valid && this.senha.valid
+    
   }
 
 }
---
+++ src/app/config/api.config.ts
@@ -0,0 +1,3 @@
+export const API_CONFIG ={
+    baseUrl : 'htttps://helpdesk-prod.herokuapp.com'
+}
